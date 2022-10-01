4545a3cada09c33f489a790ba3afc2fd588ccbfb
Nicholas Chai <nicholas.chai@outook.com>
2022-10-01 15:37:20 +0800

    implemented the recipe list

--- Recipes/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Recipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Chicken with pomegranate molasses", "wow what a terrible comibination of flavours!", "https://www.goodfood.com.au/content/dam/images/h/2/6/r/y/7/image.related.wideLandscape.940x529.h26u9c.png/1664508421879.jpg"),
+    new Recipe("Chicken with pomegranate molasses", "wow what a terrible comibination of flavours!", "https://www.goodfood.com.au/content/dam/images/h/2/6/r/y/7/image.related.wideLandscape.940x529.h26u9c.png/1664508421879.jpg")
+  ];
 
   constructor() { }
 
